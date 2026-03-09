03e4a4b0e6b180a5d0cd05f59e614ec9d9c0a6c0
scholar7r <scholar7r@outlook.com>
2026-03-09 13:31:31 +0800

    refactor(jwt): support singing method (#10)

--- jwt/jwt_test.go
+++ jwt/jwt_test.go
@@ -13,7 +13,7 @@ type userClaims struct {
 	Name string
 }
 
-var j = mjwt.New[userClaims]("secret")
+var j = mjwt.New[userClaims]("secret", jwt.SigningMethodHS256)
 
 func TestJWT_GenerateAndParse(t *testing.T) {
 	token, err := j.Generate(&mjwt.Claims[userClaims]{
@@ -57,5 +57,22 @@ func TestJWT_ParseInvalid(t *testing.T) {
 
 	if parsed != nil {
 		t.Fatalf("expected parsed to be nil, got %+v", parsed)
+	}
+}
+
+func TestJWT_WrongSigningMethod(t *testing.T) {
+	j1 := mjwt.New[userClaims]("secret", jwt.SigningMethodHS256)
+	j2 := mjwt.New[userClaims]("secret", jwt.SigningMethodHS384)
+
+	token, err := j1.Generate(&mjwt.Claims[userClaims]{
+		Data: &userClaims{ID: 1},
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = j2.Parse(token)
+	if err == nil {
+		t.Fatal("expected signing method mismatch error")
 	}
 }
